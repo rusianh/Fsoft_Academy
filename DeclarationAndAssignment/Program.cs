dbdcee4c00bebe1574889474d7d18de37ec6b81b
VietAnhVu <vva1994@gmail.com>
2022-01-26 23:30:54 +0700

    :)

---
+++ DeclarationAndAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeclarationAndAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x;
+            //int @int;
+
+            //int from = 0;
+            //Console.WriteLine("from: " + from);
+
+            //String matchInfo = "cr7)*)*(*)($%&%& vs m10&^&^&^^*";
+            //var digits = from c in matchInfo
+            //             where Char.IsDigit(c) || char.IsWhiteSpace(c) || char.IsLetter(c)
+            //             select c;
+            //foreach (var digit in digits)
+            //{
+            //    Console.Write(digit);
+            //}
+            //Console.ReadKey();
+        }
+        
+    }
+
+    
+}
